03e394affa41cb029a8d14e63062bd3d3b4608d2
Iosif <shapovalov.iosif@yandex.ru>
2020-01-03 22:45:12 +0300

    2.17.5

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-let user = {};
-user.name = "Joshn";
-user.surname = "Smith";
-
-function isEmpty(obj) {
-   for (let key in obj) {
-      return flase;
-   }
-   return true;
+function Human (gender, age, name){
+   this.gender = gender;
+   this.age = age;
+   this.name = name;
+   this.sayName = function() {
+alert(this.name +" - это моё имя");
+   };
 }
-console.log(isEmpty(user));
+
+let Ivan = new Human("Мужик",20,"Иван");
+console.log(Ivan);
+Ivan.sayName();
+let Petr = new Human("Мужик",20,"Пёртр");
+console.log(Petr);
+Petr.sayName();
+
