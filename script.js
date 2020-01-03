bee9c498aa7c2a685af73324cccf9e508a947cba
Iosif <shapovalov.iosif@yandex.ru>
2020-01-03 18:55:03 +0300

    2.17.4

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ let user = {};
 user.name = "Joshn";
 user.surname = "Smith";
 
-function isEmpty(obj){
- for (let key in obj){
-     return flase;
-     
- };
- return true;
-    console.log(isEmpty(user));
+function isEmpty(obj) {
+   for (let key in obj) {
+      return flase;
+   }
+   return true;
 }
+console.log(isEmpty(user));
